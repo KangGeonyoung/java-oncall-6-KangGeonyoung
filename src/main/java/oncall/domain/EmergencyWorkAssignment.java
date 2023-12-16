4b96ce9977911bcde420c753a0aad7b4875b6a93
KangGeonyoung <kky6335@gmail.com>
2023-12-16 15:28:33 +0900

    feat(EmergencyWorkAssignment): 평일 및 휴일 비상근무 배정 기능 - assignWhenWeekday(int month, String day): 평일일 때 평일 명단에 있는 사원 배정 - assignWhenHoliday(String day): 휴일일 때 휴일 명단에 있는 사원 배정

--- src/main/java/oncall/domain/EmergencyWorkAssignment.java
+++ src/main/java/oncall/domain/EmergencyWorkAssignment.java
@@ -10,18 +10,47 @@ public class EmergencyWorkAssignment {
     private static List<String> weekdayMember;
     private static List<String> holidayMember;
     private static List<String> finalAssignmentResult;
-    private int month;
-    private String day;
+    private static int month;
+    private static int date;
+    private static String day;
 
     public EmergencyWorkAssignment(List<String> weekdayMember, List<String> holidayMember, int month, String day) {
         this.weekdayMember = weekdayMember;
         this.holidayMember = holidayMember;
         this.finalAssignmentResult = new ArrayList<>();
         this.month = month;
+        this.date = 1;
         this.day = day;
     }
 
+    public static void startWorkAssignment(int month, String day) {
+        // 평일일때 평일 사원 명단에서 추출
+        assignWhenWeekday(month, day);
 
+        // 휴일 일때 휴일 사원 명단에서 추출
+        assignWhenHoliday(day);
+    }
+
+
+    private static void assignWhenHoliday(String day) {
+        if (!Date.isWeekday(day)) {
+            finalAssignmentResult.add(holidayMember.get(0));
+            holidayMember.remove(0);
+        }
+    }
+
+    private static void assignWhenWeekday(int month, String day) {
+        if (Date.isWeekday(day)) {
+            // 평일인데 공휴일인 경우
+            if (Date.isHoliday(month, date)) {
+                finalAssignmentResult.add(holidayMember.get(0));
+                holidayMember.remove(0);
+                return;
+            }
+            finalAssignmentResult.add(weekdayMember.get(0));
+            weekdayMember.remove(0);
+        }
+    }
 
     public static void isAssignedOnce(List<String> weekdayMember, List<String> holidayMember) {
         for (int i = 0; i < weekdayMember.size(); i++) {
