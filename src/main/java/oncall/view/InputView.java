6b1f2f3fec302f1d527f8484c853f975a888b348
KangGeonyoung <kky6335@gmail.com>
2023-12-16 14:00:52 +0900

    feat(InputView): 비상 근무 배정 월과 시작 요일 입력 받기 - readDate(): 사용자로부터 비상 근무 배정 월과 시작 요일 입력 받기

---
+++ src/main/java/oncall/view/InputView.java
@@ -0,0 +1,12 @@
+package oncall.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public static String readDate() {
+        System.out.print("비상 근무를 배정할 월과 시작 요일을 입력하세요> ");
+        return Console.readLine();
+    }
+
+}
